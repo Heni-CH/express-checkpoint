8d5d73d5ca9dce90903d22591bfc6aa7f91f4e6e
Heni <Heni_ch@hotmail.com>
2022-05-25 10:32:21 +0100

    express

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const logger = require("./middleware/logger");
+
+const app = express();
+
+
+
+
+app.use(logger);
+
+app.use(express.json());
+
+app.use(express.static(__dirname + "/public"));
+
+const PORT = 5000;
+
+app.listen(PORT, (error) => {
+
+    error ? console.log(error) : console.log(`The server is listening at ${PORT}`);
+
+});
+ 
+
+
